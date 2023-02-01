5e5b6a22fbb673147841deedc59c06cb693b5357
Tran Quoc Khanh Nguyen <tqknguyen@myseneca.ca>
2023-01-31 19:43:09 -0500

    update transporter sendMail

--- routes/TrackingRoute.js
+++ routes/TrackingRoute.js
@@ -69,36 +69,41 @@ route.post("/api/tracking/contact", async(req, res, next) => {
   };
 
   // // Send Mail
-  await transporter.sendMail(mail, async(err, data) => {
-    if (err) {
-      res.status(500).json("Error during sending")
-    } else {
-     res.status(200).json("Successful send the email")
-    }
-
-    // Send auto-reply mail
-    await transporter.sendMail(
-      {
-        from: process.env.EMAIL_USER,
-        to: email,
-        subject: "Thank you for your email",
-        html: `<span>Dear Value Customer, </span>
-          <p>Order Tracking App has received your email.
-          Our Transportation Service team will get back to you in 24 hours
-          </p>
-          <h5>Best regards</h5>
-          <h5>Order Tracking Management Team</h5>
-        `,
-      },
-      (err, info) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(`Message sent: ${info.response}`);
-        }
+  await new Promise((resolve, reject)=>{
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        res.status(500).json("Error during sending")
+        reject();
+      } else {
+       res.status(200).json("Successful send the email")
+        resolve();
       }
-    );
-  });
+  
+      // Send auto-reply mail
+      transporter.sendMail(
+        {
+          from: process.env.EMAIL_USER,
+          to: email,
+          subject: "Thank you for your email",
+          html: `<span>Dear Value Customer, </span>
+            <p>Order Tracking App has received your email.
+            Our Transportation Service team will get back to you in 24 hours
+            </p>
+            <h5>Best regards</h5>
+            <h5>Order Tracking Management Team</h5>
+          `,
+        },
+        (err, info) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(`Message sent: ${info.response}`);
+          }
+        }
+      );
+    });
+  })
+
 });
 
 
