bd7f61e33fe08f4da195f0409f623f025c1cb4b3
Tran Quoc Khanh Nguyen <tqknguyen@myseneca.ca>
2023-01-31 22:15:03 -0500

    Fix nodemailer

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,52 +1,52 @@
-const nodemailer = require('nodemailer');
-const { translateAliases } = require('../models/trackingModel');
+const nodemailer = require("nodemailer");
+const { translateAliases } = require("../models/trackingModel");
 
 // Configure transport
-const sendEmail = async(subject, message, send_to, sent_from, reply_to)=>{
-    var transport = {
-        host: process.env.EMAIL_HOST,
-        port: 587,
-        auth:{
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        },
-        tls: {
-            rejectUnauthorized: false,
-          },
-    }
-    
-    // Option for sending email
-    const options ={
-        from: sent_from,
-        to: send_to,
-        replyTo: reply_to,
-        subject: subject,
-        html: `<span>Dear Value Customer, </span>
-        <p>Order Tracking App has received your email.
-        Our Transportation Service team will get back to you in 24 hours
-        </p>
-        <p>Your message is : ${message}</p>
-        <h5>Best regards</h5>
-        <h5>Order Tracking Management Team</h5>
-      `,
-      
-    }
-    // Create Transport
-    var transporter = nodemailer.createTransport(transport);
-    transporter.sendMail(options,(err,info)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            console.log(info);
-        }
-    })
+const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
+  var transport = {
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  };
 
-    transporter.sendMail()
-}
+  // Option for sending email
+  const options = {
+    from: sent_from,
+    to: send_to,
+    replyTo: reply_to,
+    subject: subject,
+    html: `
+        <html>
+            <body>
+                <span>Dear Value Customer, </span>
+                <p>Order Tracking App has received your email.
+                Our Transportation Service team will get back to you in 24 hours
+                </p>
+                <p>Your message is : ${message}</p>
+                <h5>Best regards</h5>
+                <h5>Order Tracking Management Team</h5>
+            </body>
+        </html>
+        
+      `,
+  };
+  // Create Transport
+  var transporter = nodemailer.createTransport(transport);
+  transporter.sendMail(options, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+
+  transporter.sendMail();
+};
 
 module.exports = sendEmail;
-
-
-
-
