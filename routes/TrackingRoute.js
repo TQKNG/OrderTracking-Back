afb798afa7215387755e26dd5ba1a04a1c8e5447
Tran Quoc Khanh Nguyen <tqknguyen@myseneca.ca>
2023-03-01 12:54:24 -0500

    Check tracking route

--- routes/TrackingRoute.js
+++ routes/TrackingRoute.js
@@ -1,15 +1,11 @@
 const {getAllTracking, getTrackingById, addTracking, deleteTracking, updateTracking} = require("../controllers/trackingController")
 const route = require("express").Router();
-const sendMail = require("../utils/sendEmail");
 
 route.get("/", getAllTracking);
 route.get("/:id", getTrackingById);
 route.post("/", addTracking);
 route.delete("/:id", deleteTracking);
 route.put("/:id", updateTracking);
-
-// email route
-
 
 module.exports = route;
 
