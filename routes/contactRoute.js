4a399f254ff7655df8a560db9caf4dc505e94461
Tran Quoc Khanh Nguyen <tqknguyen@myseneca.ca>
2023-03-01 17:54:34 -0500

    Test again with Capitalize filename and .js

--- routes/contactRoute.js
+++
@@ -1,5 +0,0 @@
-const router = require("express").Router();
-const sendMail = require("../utils/sendEmail");
-
-router.post("/", sendMail);
-module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv').config();
 const HTTP_PORT = process.env.HTTP_PORT || 5050;
-const trackingService = require('./routes/trackingRoute.js');
-const contactService = require('./routes/contactRoute.js');
+const trackingService = require('./routes/TrackingRoute.js');
+// const contactService = require('./routes/contactRoute.js');
 // const userRoute = require('./routes/userRoute')
 const mongoose = require('mongoose');
 
@@ -14,7 +14,7 @@ app.use(cors({
     credentials:true,
 }));
 app.use('/api/tracking',trackingService);
-app.use('/api/contact',contactService);
+// app.use('/api/contact',contactService);
 // app.use('/api/user',userRoute);
 
 app.get('/',(req,res)=>{
