efe3db06d35dea457e2e20dc95233dc7dbe22752
Tran Quoc Khanh Nguyen <tqknguyen@myseneca.ca>
2023-03-01 18:04:33 -0500

    Update getTrackingById

--- routes/TrackingRoute.js
+++ routes/TrackingRoute.js
@@ -1,8 +1,8 @@
-const {getAllTracking, addTracking, deleteTracking, updateTracking} = require("../controllers/trackingController")
+const {getAllTracking, addTracking, getTrackingById, deleteTracking, updateTracking} = require("../controllers/trackingController")
 const router = require("express").Router();
 
 router.get("/", getAllTracking);
-// route.get("/:id", getTrackingById);
+router.get("/:id", getTrackingById);
 router.post("/", addTracking);
 router.delete("/:id", deleteTracking);
 router.put("/:id", updateTracking);
