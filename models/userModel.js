4df239442b4a740f83a8fc01dba0e343e5bcd90b
Tran Quoc Khanh Nguyen <tqknguyen@myseneca.ca>
2023-03-01 20:31:51 -0500

    Order Tracking Backend Version 2

--- models/userModel.js
+++ models/userModel.js
@@ -17,8 +17,6 @@ userSchema.pre("save",async function(next){
     if(!this.isModified("password")){
         return next();
     }
-
-    // Hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(this.password, salt);
     this.password = hashedPassword;
